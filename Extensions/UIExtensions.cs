41ed4e8832004e68656318d23c8b4a4c4e7918d7
Max <hppp101@gmail.com>
2023-09-27 07:55:38 +0300

    Removed GetButton

--- Extensions/UIExtensions.cs
+++ Extensions/UIExtensions.cs
@@ -6,15 +6,6 @@ using UnityEngine.Events;
 
 namespace Beret
 {
-    public enum BtnEventType
-    {
-        NONE = 0,
-        PRESS,
-        RELEASE,
-        CLICK,
-        TOGGLE
-    }
-
     public static class UIExtensions
     {
         public static void SetValue<T>(this RectTransform rect, string element, T obj)
@@ -47,39 +38,6 @@ namespace Beret
                     Debug.LogError($"Unsupported Type: {typeof(T)}");
                     break;
             }
-        }
-
-        public static UnityEvent GetButton(this RectTransform rect, string element, BtnEventType evtType)
-        {
-            Transform uiElement = rect.Find(element);
-            if (uiElement == null)
-            {
-                Debug.LogError($"Failed to find {element}");
-                return null;
-            }
-
-            if (!uiElement.TryGetComponent(out Selectable sel))
-            {
-                Debug.LogError($"Failed to find a button on {element}");
-                return null;
-            }
-
-            switch (evtType)
-            {
-                case BtnEventType.PRESS:
-                    break;
-                case BtnEventType.RELEASE:
-                    break;
-                case BtnEventType.CLICK:
-                    break;
-                case BtnEventType.TOGGLE:
-                    break;
-                case BtnEventType.NONE:
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(evtType), evtType, null);
-            }
-			
-			return null;
         }
     }
 }
